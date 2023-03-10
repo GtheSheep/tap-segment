76f14f2b0d9397964b673b2a8e76f9a400fd84f8
Gary James <gary.james19@gmail.com>
2023-03-10 10:04:06 +0100

    start_date

--- tap_segment/streams.py
+++ tap_segment/streams.py
@@ -111,7 +111,7 @@ class SourceMTUUUsageDailyStream(SegmentStream):
         start_date = datetime.datetime.strptime(parse_qs(urlparse(response.request.url).query)['period'][0], '%Y-%m-%dT%H:%M:%SZ')
         this_month = datetime.datetime.today().replace(day=1)
         if not next_page_token and start_date.date() < this_month.date():
-            next_page_token = add_months(this_month, 1)
+            next_page_token = add_months(start_date, 1)
         return next_page_token
 
     def get_url_params(
@@ -169,7 +169,7 @@ class WorkspaceMTUUUsageDailyStream(SegmentStream):
         start_date = datetime.datetime.strptime(parse_qs(urlparse(response.request.url).query)['period'][0], '%Y-%m-%dT%H:%M:%SZ')
         this_month = datetime.datetime.today().replace(day=1)
         if not next_page_token and start_date.date() < this_month.date():
-            next_page_token = add_months(this_month, 1)
+            next_page_token = add_months(start_date, 1)
         return next_page_token
 
     def get_url_params(
